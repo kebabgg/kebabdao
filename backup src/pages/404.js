1b3137fa346e092985a4c9e1a8d70f3cc75893ab
kebabgg <152669628+kebabgg@users.noreply.github.com>
2024-01-09 20:24:01 +0300

    Update 404.js

--- backup src/pages/404.js
+++ backup src/pages/404.js
@@ -7,7 +7,7 @@ const Custom404 = () => {
   return (
     <>
       <Head>
-        <title>404 | WebXDAO</title>
+        <title>404 | KEBAB.GG</title>
       </Head>
       <div className="min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
         <div className="mx-auto max-w-max">
