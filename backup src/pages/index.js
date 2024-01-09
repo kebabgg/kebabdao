4b5fe6ed09f09b4383c73663bf8092f303d8b2a8
kebabgg <152669628+kebabgg@users.noreply.github.com>
2024-01-09 20:23:29 +0300

    Update index.js

--- backup src/pages/index.js
+++ backup src/pages/index.js
@@ -5,17 +5,17 @@ export default function Home({ articles }) {
   return (
     <>
       <Head>
-        <title>WebXDAO - OpenSource Community</title>
+        <title>KEBAB.GG Community</title>
         <meta
           name="description"
           content="We are an OpenSource community working around the future of the web. Learn Blockchain technology together."
         />
-        <meta name="keywords" content="WebXDAO,Web,Web3,Blockchain,OpenSource,DAO" />
+        <meta name="keywords" content="KEBAB.GG,Web,Web3,Blockchain,OpenSource,DAO" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="author" content="WebXDAO community" />
-        <meta property="og:title" content="WebXDAO - OpenSource Community" />
+        <meta name="author" content="KEBAB.GG community" />
+        <meta property="og:title" content="KEBAB.GG - OpenSource Community" />
         <meta property="og:image" content="https://github.com/WebXDAO.png" />
-        <meta property="og:url" content="https://webxdao.xyz" />
+        <meta property="og:url" content="https://kebab.gg" />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en_US" />
         <meta
@@ -25,9 +25,9 @@ export default function Home({ articles }) {
         <meta name="language" content="English" />
         <meta name="linkedin:profile" content="" />
         <meta property="instagram:username" content="" />
-        <meta name="twitter:site" content="https://twitter.com/WebXDAO" />
+        <meta name="twitter:site" content="https://twitter.com/KEBABgg_" />
         <meta property="discord:server" content="835424705410236427" />
-        <meta property="discord:invite" content="https://discord.webxdao.xyz/" />
+        <meta property="discord:invite" content="https://discord.gg/kebab" />
       </Head>
       <div className="flex h-screen flex-col">
         <Hero />
