ab40e624369b46d4871d9e1caec2ec5fd74dc3bd
kebabgg <152669628+kebabgg@users.noreply.github.com>
2024-01-10 00:30:00 +0300

    Update DiscordButton.js

--- src/components/v1/Home/HeroSection/DiscordButton.js
+++ src/components/v1/Home/HeroSection/DiscordButton.js
@@ -9,9 +9,9 @@ const DiscordButton = () => {
         className="bg-cyber-webx focus:ring-cyber-webx inline-flex items-center justify-center rounded-md border border-transparent px-4 py-3 text-base font-medium text-white/80 shadow-sm transition-all duration-100 hover:border-white/80 hover:bg-gradient-to-r hover:from-pink-700 hover:to-pink-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 1 }}
-        href="https://discord.webxdao.xyz/"
+        href="discord.gg/kebab/"
       >
-        Join WebXDAO
+        Join KEBAB.GG
         <FaDiscord className="-mr-1 ml-3 h-5 w-5" aria-hidden="true" />
       </motion.a>
     </>
